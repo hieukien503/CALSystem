e682ffcd2fecf392655d9e7e0e20e86f24f2e3d1
Chilly On <chiongxanh2004@gmail.com>
2025-05-17 00:29:40 +0700

    Add web

---
+++ src/components/Footer.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+
+const Footer = () => {
+    return (
+        <footer>
+            <div className="left">
+                COPYRIGHT &#169; All rights reserved.
+            </div>
+            <div className="right">
+                <nav>
+                    <ul className="navi-right">
+                        <li><a href="index.php?page=contact">Top page</a></li>
+                        <li><a href="index.php?page=contact">Login/Signup</a></li>
+                    </ul>
+                </nav>
+            </div>
+        </footer>
+    );
+};
+
+export default Footer;
