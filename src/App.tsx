7ff7a1e701f68f4ca063326a3417dfd694c26783
Chilly On <chiongxanh2004@gmail.com>
2025-07-11 15:53:11 +0700

    Add more website for login and profile

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,32 @@
 import React from 'react';
 import Header from './components/Header';
 import Footer from './components/Footer';
+import Home from './Home';
+import CalApp from './CalApp';
+import Login from './Login';
+import ForgotPassword from './ForgotPassword';
+import SignUp from './SignUp';
+import ProfilePage from './ProfilePage';
+import SearchResults from './SearchResults';
 import './index.css'; // Use Tailwind's CSS instead of App.css
 import 'bootstrap/dist/css/bootstrap.min.css';
-import KonvaCanvas from './components/KonvaRender';
+
+
 
 function App() {
     return (
         <div className="flex flex-col min-h-screen">
             <Header />
-            <h1 className="upper-main" color="#46443f">
-                2D Geometry
-            </h1>
-            <main className="outer-main">
-                <div className="inner-main text-center text-gray-600 text-xl">
-                    <KonvaCanvas width={window.innerWidth} height={window.innerHeight * 0.6} background_color='#ffffff' />
-                </div>
-            </main>
+            <CalApp />
+
+            {/*<ProfilePage />*/}
+            {/*<SearchResults />*/}
+            {/*<Home />*/}
+            {/*<SignUp />*/}
             <Footer />
         </div>
     );
 }
 
 export default App;
+                   
---
+++ src/SignUp.tsx
@@ -0,0 +1,88 @@
+import React from 'react';
+import './index.css'; // Use Tailwind's CSS instead of App.css
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+const SignUp = () => {
+    return (
+        <div>
+            <h1 className="upper-main"
+                style={{
+                    backgroundColor: "#9992B9"
+                }}
+            >
+                Login
+            </h1>
+            <div className="inner-main text-gray-600 align-items-center text-xl"
+                style={{
+                    height: "100%",
+                    margin: "50px"
+                }}
+            >
+                <div className="card shadow">
+                    <div className="d-flex flex-column card-body"
+                        style={{
+                            width: "600px"
+                        }}
+                    >
+                        <div className="d-flex flex-column text-center">
+                            <div className="fw-bold fs-3"
+                            >Sign up</div>
+                            <div>Create your Geometry Learning account</div>                   
+                        </div>
+
+                        <form action="php/login.php" method="POST" id="form">
+
+                            <div className="d-flex flex-row justify-content-between">
+                                <div className="mb-3"
+                                    style={{
+                                        width: "250px"
+                                    } }
+                                >
+                                    <label htmlFor="email" className="form-label">First name:</label>
+                                    <input type="email" className="form-control" name="email" id="email"
+                                        required />
+                                </div>
+
+                                <div className="mb-3"
+                                    style={{
+                                        width: "250px"
+                                    }}
+                                >
+                                    <label htmlFor="email" className="form-label">Last name:</label>
+                                    <input type="email" className="form-control" name="email" id="email"
+                                        required />
+                                </div>
+                            </div>
+
+                            <div className="mb-3">
+                                <label htmlFor="email" className="form-label">Email:</label>
+                                <input type="email" className="form-control" name="email" id="email"
+                                    placeholder="your.email@example.com" required />
+                            </div>
+
+                            <div className="mb-3">
+                                <label htmlFor="password" className="form-label">Password:</label>
+                                <input type="password" className="form-control" name="password" id="password" required />
+                            </div>
+
+                            <div className="mb-3">
+                                <label htmlFor="password" className="form-label">Confirm Password:</label>
+                                <input type="password" className="form-control" name="password" id="password" required />
+                            </div>
+
+                            <button type="submit" className="btn btn-dark w-100">Submit</button>
+                        </form>
+                        <div className="d-flex flex-row align-self-center text-center gap-2">
+                            <div>Already have an account?</div>
+                            <a href="#" className="accent-blue">Login</a>
+                        </div>
+                    </div>
+                </div>
+                
+            </div>
+        </div>
+    );
+};
+
+export default SignUp;
+                   
