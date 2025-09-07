e2506519a371b4e20c4ebc8f1d53732f6a795c5f
Chilly On <chiongxanh2004@gmail.com>
2025-09-07 18:49:08 +0700

    Add more website for login and profile

--- src/CalApp.tsx
+++ src/CalApp.tsx
@@ -1,16 +1,17 @@
 import React from 'react';
 import './index.css'; // Use Tailwind's CSS instead of App.css
 import 'bootstrap/dist/css/bootstrap.min.css';
-import KonvaCanvas from './components/KonvaRender';
+//import KonvaCanvas from './components/KonvaRender';
 
 const CalApp = () => {
     return (
         <main className="outer-main">
             <div className="inner-main text-center text-gray-600 text-xl">
-                <KonvaCanvas width={window.innerWidth} height={window.innerHeight * 0.7} background_color='#ffffff' />
+                
             </div>
         </main>
     );
 };
 
 export default CalApp;
+//<KonvaCanvas width={window.innerWidth} height={window.innerHeight * 0.7} background_color='#ffffff' />
