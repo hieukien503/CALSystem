60abf8a0dd673acef30ab6d73a5377dbcbd52e46
Chilly On <chiongxanh2004@gmail.com>
2025-09-15 13:33:36 +0700

    Fix footer gaps

--- src/CalApp.tsx
+++ src/CalApp.tsx
@@ -1,13 +1,13 @@
 import React from 'react';
 import './index.css'; // Use Tailwind's CSS instead of App.css
 import 'bootstrap/dist/css/bootstrap.min.css';
-import KonvaCanvas from './components/KonvaRender';
-
+//import KonvaCanvas from './components/KonvaRender';
+// <KonvaCanvas width={window.innerWidth} height={window.innerHeight * 0.7} background_color='#ffffff' />
 const CalApp = () => {
     return (
         <main className="outer-main">
             <div className="inner-main text-center text-gray-600 text-xl">
-                <KonvaCanvas width={window.innerWidth} height={window.innerHeight * 0.7} background_color='#ffffff' />
+                
             </div>
         </main>
     );
