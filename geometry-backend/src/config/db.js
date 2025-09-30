353e56ec246425b05a47919190de8c92b136ee81
FelixLa <kyphuong0120@gmail.com>
2025-09-30 12:41:11 +0700

    Search Project/User

--- geometry-backend/src/config/db.js
+++ geometry-backend/src/config/db.js
@@ -2,15 +2,16 @@ const mongoose = require("mongoose");
 
 const connectDB = async () => {
     try {
-        await mongoose.connect(process.env.MONGO_URI, {
-            useNewUrlParser: true,
-            useUnifiedTopology: true,
-        });
-        
-    } catch (err) {
-            console.error("❌ MongoDB connection failed", err.message);
-            process.exit(1);
-    }
+        console.log("process.env.MONGO_URI: ", "mongodb+srv://phuongla342_db:bcdopttll@cluster0.vtc7zgr.mongodb.net/doan_db?retryWrites=true&w=majority");
+    await mongoose.connect("mongodb+srv://phuongla342_db:bcdopttll@cluster0.vtc7zgr.mongodb.net/doan_db?retryWrites=true&w=majority", {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log("✅ MongoDB connected");
+  } catch (err) {
+    console.error("❌ MongoDB connection failed", err.message);
+    process.exit(1);
+  }
 };
 
 module.exports = connectDB;
